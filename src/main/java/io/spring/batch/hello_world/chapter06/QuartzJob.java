ba87869a277009b9873a1386c1d111c64cc6665d
woonie155 <woonie155@gmail.com>
2024-11-17 00:26:27 +0900

    basic Quartz

--- src/main/java/io/spring/batch/hello_world/chapter06/QuartzJob.java
+++ src/main/java/io/spring/batch/hello_world/chapter06/QuartzJob.java
@@ -1,47 +1,44 @@
-package io.spring.batch.hello_world.chapter05;
+package io.spring.batch.hello_world.chapter06;
 
 import io.spring.batch.hello_world.chapter04.job.DailyJobTimeStamper;
 import io.spring.batch.hello_world.chapter04.job.JobLoggerListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.JobListenerFactoryBean;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-//@Configuration
-public class JobExplorerJob {
+@Configuration
+public class QuartzJob {
+
     @Autowired
     private JobRepository jobRepository;
     @Autowired
     private PlatformTransactionManager transactionManager;
-    @Autowired
-    private JobExplorer jobExplorer;
 
     @Bean
     public Job job() {
-        return new JobBuilder("JobExplorerJob", jobRepository)
-                .start(step())
-                .incrementer(new DailyJobTimeStamper())
+        return new JobBuilder("QuartzJob", jobRepository)
+                .start(step1())
+                .incrementer(new RunIdIncrementer())
                 .listener(JobListenerFactoryBean.getListener(
                         new JobLoggerListener()))
                 .build();
     }
-    @Bean
-    public Step step() {
-        return new StepBuilder("explorerStep", jobRepository)
-                .tasklet(explorerTasklet(), transactionManager)
-                .build();
-    }
 
     @Bean
-    public Tasklet explorerTasklet() {
-        return new ExploringTasklet(this.jobExplorer);
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step1 ran!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
     }
 }
---
+++ src/main/java/io/spring/batch/hello_world/chapter06/QuartzSchedulerConfiguration.java
@@ -0,0 +1,31 @@
+package io.spring.batch.hello_world.chapter06;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzSchedulerConfiguration { //스케줄러
+
+    @Bean
+    public JobDetail quartzJobDetail() {
+        return JobBuilder.newJob(BatchScheduledJobBean.class)
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger jobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(5).withRepeatCount(4);
+
+        return TriggerBuilder.newTrigger()
+                .forJob(quartzJobDetail())
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+}
