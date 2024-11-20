43e39cbaa3c518e0d5824ee6bf1e5449fb938566
woonie155 <woonie155@gmail.com>
2024-11-21 02:23:44 +0900

    RepositoryItemReader (use SpringDataRepository)

---
+++ src/main/java/io/spring/batch/hello_world/chapter07_reader/JPAItemReader/CustomerRepository.java
@@ -0,0 +1,11 @@
+package io.spring.batch.hello_world.chapter07_reader.JPAItemReader;
+
+import io.spring.batch.hello_world.domain.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Page<Customer> findByCity(String city, Pageable pageable);
+}
--- src/main/java/io/spring/batch/hello_world/chapter07_reader/JPAItemReader/SpringDataRepositoryJob.java
+++ src/main/java/io/spring/batch/hello_world/chapter07_reader/JPAItemReader/SpringDataRepositoryJob.java
@@ -1,13 +1,8 @@
-package io.spring.batch.hello_world.chapter07_reader.StoredProcedureItemReader;
+package io.spring.batch.hello_world.chapter07_reader.JPAItemReader;
 
 import io.spring.batch.hello_world.chapter04.job.JobLoggerListener;
-import io.spring.batch.hello_world.chapter07_reader.JPAItemReader.CustomerByCityQueryProvider;
-import io.spring.batch.hello_world.chapter07_reader.JdbcItemReader.CustomerRowMapper;
 import io.spring.batch.hello_world.domain.Customer;
-import jakarta.persistence.EntityManagerFactory;
-import java.sql.Types;
 import java.util.Collections;
-import javax.sql.DataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -16,20 +11,17 @@ import org.springframework.batch.core.listener.JobListenerFactoryBean;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.StoredProcedureItemReader;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
-import org.springframework.batch.item.database.builder.StoredProcedureItemReaderBuilder;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
-import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
-//@Configuration
-public class StoredProcedureJob {
+@Configuration
+public class SpringDataRepositoryJob {
 
     @Autowired
     private JobRepository jobRepository;
@@ -38,7 +30,7 @@ public class StoredProcedureJob {
 
     @Bean
     public Job job() {
-        return new JobBuilder("StoredProcedureJob", jobRepository)
+        return new JobBuilder("SpringDataRepositoryJob", jobRepository)
                 .start(copyFileStep())
                 .listener(JobListenerFactoryBean.getListener(new JobLoggerListener()))
                 .build();
@@ -53,22 +45,21 @@ public class StoredProcedureJob {
                 .build();
     }
 
-
-    @Bean
-    @StepScope
-    public StoredProcedureItemReader<Customer> customerItemReader(
-            DataSource dataSource,
+	@Bean
+	@StepScope
+	public RepositoryItemReader<Customer> customerItemReader(
+            CustomerRepository repository,
             @Value("#{jobParameters['city']}") String city) {
 
-		return new StoredProcedureItemReaderBuilder<Customer>()
+		return new RepositoryItemReaderBuilder<Customer>()
 				.name("customerItemReader")
-				.dataSource(dataSource)
-				.procedureName("customer_list")
-				.parameters(new SqlParameter[]{new SqlParameter("cityOption", Types.VARCHAR)})
-				.preparedStatementSetter(new ArgumentPreparedStatementSetter(new Object[] {city}))
-				.rowMapper(new CustomerRowMapper())
+				.arguments(Collections.singletonList(city))
+				.methodName("findByCity")
+				.repository(repository)
+				.sorts(Collections.singletonMap("lastName", Sort.Direction.ASC))
 				.build();
-    }
+	}
+
 
     @Bean
     public ItemWriter itemWriter() {
