216cdbad48f2ae0663dd969d29f799cb52c1cf09
woonie155 <woonie155@gmail.com>
2024-11-21 03:18:16 +0900

    ItemStreamReader impl

--- src/main/java/io/spring/batch/hello_world/chapter07_reader/ItemStreamReader/ItemStreamReaderJob.java
+++ src/main/java/io/spring/batch/hello_world/chapter07_reader/ItemStreamReader/ItemStreamReaderJob.java
@@ -1,30 +1,21 @@
-package io.spring.batch.hello_world.chapter07_reader.ItemReaderAdapter;
+package io.spring.batch.hello_world.chapter07_reader.ItemStreamReader;
 
 import io.spring.batch.hello_world.chapter04.job.JobLoggerListener;
-import io.spring.batch.hello_world.chapter07_reader.JPAItemReader.CustomerRepository;
 import io.spring.batch.hello_world.domain.Customer;
-import java.util.Collections;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.listener.JobListenerFactoryBean;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.adapter.ItemReaderAdapter;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
-//@Configuration
-public class ItemReaderAdapterJob {
+@Configuration
+public class ItemStreamReaderJob {
 
     @Autowired
     private JobRepository jobRepository;
@@ -33,7 +24,7 @@ public class ItemReaderAdapterJob {
 
     @Bean
     public Job job() {
-        return new JobBuilder("ItemReaderAdapterJob", jobRepository)
+        return new JobBuilder("ItemStreamReaderJob", jobRepository)
                 .start(copyFileStep())
                 .listener(JobListenerFactoryBean.getListener(new JobLoggerListener()))
                 .build();
@@ -43,19 +34,16 @@ public class ItemReaderAdapterJob {
     public Step copyFileStep() {
         return new StepBuilder("copyFileStep", jobRepository)
                 .<Customer, Customer>chunk(10, transactionManager)
-                .reader(customerItemReader(null))
+                .reader(customerItemReader())
                 .writer(itemWriter())
                 .build();
     }
 
-
     @Bean
-    @StepScope
-    public ItemReaderAdapter<Customer> customerItemReader(CustomerService customerService){
-        ItemReaderAdapter adapter = new ItemReaderAdapter();
-        adapter.setTargetObject(customerService);
-        adapter.setTargetMethod("getCustomer");
-        return adapter;
+    public CustomerItemReader customerItemReader(){
+        CustomerItemReader customerItemReader= new CustomerItemReader();
+        customerItemReader.setName("customerItemReader");
+        return customerItemReader;
     }
 
     @Bean
